c4ee22c8e44ba8c543e2784e66fcb5f8250b1e82
 jeweler23 <lopatin.lopatin2011@gmail.com>
2024-04-15 19:28:55 +0300

    feat: fix bag dev server

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
